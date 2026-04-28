49bba2358beb244bb04efbb05db034e036b31380
dcgator37 <dcgator8@hotmail.com>
2026-04-27 21:42:22 -0400

    fixed react-router again x3

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router';
 import Todo from './Todo';
 
 
